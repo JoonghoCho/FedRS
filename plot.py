8316dc09349811a1e754e325b8466c38d275fe1c
JoonghoCho-AISL <joongho.sejong@gmail.com>
2022-04-12 06:26:05 +0000

    12 April

---
+++ plot.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0,10,1000)
+
+plt.plot(x)
+plt.show()
--- src/client.py
+++ src/client.py
@@ -14,17 +14,8 @@ import utils
 from sklearn.model_selection import train_test_split
 
 
-update_config = {
-    'num_classes' : 10,
-    'local_epochs' : 3,
-    'local_batch_size' : 100,
-    'img_shape' : (100, 100, 3),
-    'learning_rate' : 0.001,
-}
 
-client_config = {
-    'num' : 3,
-}
+
 
 class Client():
     def __init__(self, client_id : int, update_config : dict, weights, round : int, c):
@@ -46,63 +37,76 @@ class Client():
     #         myDict = pickle.load(fo, encoding='latin1')
     #     return myDict
 
+    @tf.function
+    def train_step(self,x, y):
+        with tf.GradientTape() as tape:
+            logits = self.model(x, training=True)
+            loss_value = self.loss_fn(y, logits)
+        grads = tape.gradient(loss_value, self.model.trainable_weights)
+        # print(grads)
+        # Scaffold
+        if type(self.global_c) == int:
+            self.global_c = list()
+            for i in range(len(grads)):
+                self.global_c.append(tf.zeros_like(grads[i]))
+            self.global_c = tf.convert_to_tensor(self.global_c)
+            self.local_c = copy.deepcopy(self.global_c)
+            
+            # self.global_c = tf.zeros_like(grads)
+        # if type(self.local_c) == int:
+        #     self.local_c = list()
+        #     for i in range(len(grads))
+        #     self.local_c = tf.zeros_like(grads)
+        grads = grads + self.global_c - self.local_c
+        self.optimizer.apply_gradients(zip(grads, model.trainable_weights))
+        self.train_acc_metric.update_state(y, logits)
+        return loss_value
+
+    @tf.function
+    def test_step(self, x, y):
+        self.val_logits = self.model(x, training = False)
+        self.val_acc_metric.update_state(y, self.val_logits)
+
     def update_model(self):
         self.model = model.get_convnext_model(input_shape=self.update_config['img_shape'],
                                                     num_classes = self.update_config['num_classes'])
         self.model.set_weights(self.pre_weights)
-        # self.model.compile(
-        #     optimizer= tf.keras.optimizers.Adam(learning_rate = self.update_config['learning_rate']),
-        #     loss = 'categorical_crossentropy',
-        #     metrics = ['acc']
-        # )
 
-        # self.model.fit(
-        #     self.train_DS,
-        #     batch_size = self.update_config['local_batch_size'],
-        #     epochs = self.update_config['local_epochs'],
-        #     validation_data = self.val_DS,
-        #     verbose = 0,
-        # )
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate = self.update_config['learning_rate'])
+        self.loss_fn = tf.keras.losses.CategoricalCrossentropy()
+        self.train_acc_metric = tf.keras.metrics.CategoricalAccuracy()
+        self.val_acc_metric = tf.keras.metrics.CategoricalAccuracy()
 
-        optimizer = tf.keras.optimizers.Adam(learning_rate = self.update_config['learning_rate'])
-        loss_fn = tf.keras.losses.CategoricalCrossentropy()
-        train_acc_metric = tf.keras.metrics.CategoricalAccuracy()
-        val_acc_metric = tf.keras.metrics.CategoricalAccuracy()
         for epoch in range(self.update_config['local_epochs']):
-            print('\nStart of epoch %d '% (epoch,))
+            print("\nStart of epoch %d" % (epoch,))
             start_time = time.time()
             for step, (x_batch_train, y_batch_train) in enumerate(self.train_DS):
-                # Open a GradientTape to record the operations run
-                with tf.GradientTape() as tape:
-                    logits = self.model(x_batch_train, training = True)
-                    loss_value = loss_fn(y_batch_train, logits)
-                grads = tape.gradient(loss_value, self.model.trainable_weights)
-                # Scaffold
-                if type(self.global_c) == int:
-                    self.global_c = np.zeros_like(grads, object)
-                if type(self.local_c) == int:
-                    self.local_c = np.zeros_like(grads, object)
-                grads = grads + self.global_c - self.local_c
-                optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
+                loss_value = self.train_step(x_batch_train, y_batch_train)
 
-                # update training metric
-                train_acc_metric.update_state(y_batch_train, logits)
+                # Log every 200 batches.
                 if step % 200 == 0:
-                    print('Training loss (for one batch) at step %d: %.4f'
-                    % (step, float(loss_value)))
-                    print('Seen so far : %s samples' % ((step + 1) * 32))
-                train_acc = train_acc_metric.result()
-                print('Training acc over epoch : %.4f' % (float(train_acc),))
-                train_acc_metric.reset_states()
+                    print(
+                        "Training loss (for one batch) at step %d: %.4f"
+                        % (step, float(loss_value))
+                    )
+                    print("Seen so far: %d samples" % ((step + 1) * 32))
 
-                #run a validation loop at the end of each epoch
-                for x_batch_val, y_batch_val in self.val_DS:
-                    val_logits = self.model(x_batch_val, training = False)
-                    val_acc_metric.update_state(y_batch_val, val_logits)
-                val_acc = val_acc_metric.result()
-                val_acc_metric.reset_states()
-                print('Validation acc : %.4f' % (float(val_acc),))
-                print('Time taken: %.2fs' % (time.time() - start_time))
+            # Display metrics at the end of each epoch.
+            train_acc = self.train_acc_metric.result()
+            print("Training acc over epoch: %.4f" % (float(train_acc),))
+
+            # Reset training metrics at the end of each epoch
+            self.train_acc_metric.reset_states()
+
+            # Run a validation loop at the end of each epoch.
+            for x_batch_val, y_batch_val in self.val_DS:
+                self.test_step(x_batch_val, y_batch_val)
+
+            val_acc = self.val_acc_metric.result()
+            self.val_acc_metric.reset_states()
+            print("Validation acc: %.4f" % (float(val_acc),))
+            print("Time taken: %.2fs" % (time.time() - start_time))
+
 
         self.weights = self.model.get_weights()
         # self.weights = np.array(self.weights[0])
@@ -117,12 +121,15 @@ class Client():
     
     def cal_c(self, diff):
         self.local_c = self.local_c - self.global_c + \
-        (1/(self.update_config['learning_rate'] * self.round * diff))
+        (1/(self.update_config['learning_rate'] * self.round) * diff)
         
     def send_update(self):
         diff = self.diff_weights()
         self.cal_c(diff)
-        print(self.local_c)
+        # print(self.local_c)
+        diff_np = np.asarray(os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), ('models/clients/diff.npy')),diff)
+        np.save(diff_np)
+        print('save diff')
         return diff, self.local_c
         
         
@@ -131,10 +138,27 @@ if __name__ == '__main__':
     if gpus:
     # 텐서플로가 첫 번째 GPU만 사용하도록 제한
         try:
-            tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+            tf.config.experimental.set_visible_devices(gpus[1], 'GPU')
         except RuntimeError as e:
             # 프로그램 시작시에 접근 가능한 장치가 설정되어야만 합니다
             print(e)
+
+
+    update_config = {
+        'seed' : 0,
+        'num_classes' : 10,
+        'local_epochs' : 1,
+        'local_batch_size' : 100,
+        'img_shape' : (100, 100, 3),
+        'learning_rate' : 0.001
+    }
+
+    fed_config = {
+        'clients_num' : 3,
+        'frac_of_clients' : 100,
+        'num_of_round' : 10,
+    }
+
 
     models = model.get_convnext_model(input_shape=update_config['img_shape'],
                                                     num_classes = update_config['num_classes'])
@@ -143,8 +167,12 @@ if __name__ == '__main__':
         loss = 'categorical_crossentropy',
         metrics = ['acc']
     )
+    # print(models.summary())
 
     init_weights = models.get_weights()
-
+    # model_file_path = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), ('models/clients/convnext.h5'))
+    # model_weights_path = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), ('models/clients/convnext'))
+    # models.save(model_file_path)
+    # models.save_weights(model_weights_path)
     client = Client(client_id = 1, update_config = update_config, weights = init_weights, round = 1, c = 0)
     client.send_update()
